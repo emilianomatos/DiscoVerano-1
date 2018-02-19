f85b1a9030d7b8686c6e63d43ef4629fa7575ba8
Emiliano <emiliano_matos@hotmail.com>
2018-02-18 23:14:57 -0300

    Segunda parte de TP1. Se agregan los test

--- test/ar/edu/unlam/pb2/TestDeDisco.java
+++ test/ar/edu/unlam/pb2/TestDeDisco.java
@@ -7,8 +7,33 @@ import org.junit.Test;
 public class TestDeDisco {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testQueCalculaElPerimetroInterior(){
+		
+		Disco miDisco = new Disco(2.0,4.0);
+		Double valorActual = miDisco.calcularPerimetroInterior();
+		Double valorEsperado = 12.56;
+		
+		assertEquals(valorEsperado, valorActual, 0.01);		
+	}
+
+	@Test
+	public void testQueCalculaElPerimetroExterior() {
+		
+		Disco miDisco = new Disco(2.0,4.0);
+		Double valorActual = miDisco.calcularPerimetroExterior();
+		Double valorEsperado = 25.13;
+
+		assertEquals(valorEsperado, valorActual, 0.01);
+	}
+
+	@Test
+	public void testParaCalcularLaSuperficie(){
+		
+		Disco miDisco = new Disco(2.0,4.0);
+		Double valorActual = miDisco.calcularSuperficie();
+		Double valorEsperado = 50.26 ;
+		
+		assertEquals(valorEsperado, valorActual, 0.01);
 	}
 
 }
